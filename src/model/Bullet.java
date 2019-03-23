7ee4ff3a3c1272154768ae644bebd598e1dd4d1e
Ilnar Shafigullin <shafigullin.ik@gmail.com>
2019-03-23 19:05:15 +0300

    Class Bullet - created;

---
+++ src/model/Bullet.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Bullet {
+
+    private Point bulletPoint;
+
+    private Directions bulletDirection;
+
+    private Tank owner;
+
+    private int step;
+
+    public Bullet(Point bulletPoint, Directions bulletDirection, Tank owner, int step) {
+        this.bulletPoint = bulletPoint;
+        this.bulletDirection = bulletDirection;
+        this.owner = owner;
+        this.step = step;
+    }
+
+    public Point getBulletPoint() {
+        return bulletPoint;
+    }
+
+    public Directions getBulletDirection() {
+        return bulletDirection;
+    }
+
+    public Tank getOwner() {
+        return owner;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    private void move() {
+        switch (bulletDirection) {
+            case LEFT:
+                bulletPoint = new Point(bulletPoint.getX() - step, bulletPoint.getY());
+                break;
+            case UP:
+                bulletPoint = new Point(bulletPoint.getX(), bulletPoint.getY() - step);
+                break;
+            case RIGHT:
+                bulletPoint = new Point(bulletPoint.getX() + step, bulletPoint.getY());
+                break;
+            case DOWN:
+                bulletPoint = new Point(bulletPoint.getX(), bulletPoint.getY() + step);
+                break;
+        }
+    }
+
+
+}
