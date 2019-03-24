17de074c38710402663f53281dac10493c55b064
Ilnar Shafigullin <shafigullin.ik@gmail.com>
2019-03-24 19:36:01 +0300

    class Game - methods addBullet() and removeInactiveBullets() removed from class Game and added to class BulletController;

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.util.ArrayList;
 
 public class Game {
 
@@ -10,25 +9,10 @@ public class Game {
 
     private Field field;
 
-    private ArrayList<Bullet> bullets = new ArrayList<>();
-
     public Game(Player player1, Player player2, Field field) {
         this.player1 = player1;
         this.player2 = player2;
         this.field = field;
     }
 
-    public void addBullet(Bullet bullet){
-        bullets.add(bullet);
-    }
-
-    public void removeInactiveBullets(){
-        ArrayList<Bullet> temp = new ArrayList<>(bullets.size());
-        for (Bullet bullet: bullets) {
-            if(bullet.isBulletStatus()){
-                temp.add(bullet);
-            }
-        }
-        bullets = temp;
-    }
 }
