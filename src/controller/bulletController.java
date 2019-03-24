a6a277614e0f93f429e9eb439f59b91b8298c08f
Ilnar Shafigullin <shafigullin.ik@gmail.com>
2019-03-24 19:34:25 +0300

    class bulletController - created;

---
+++ src/controller/bulletController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import model.Bullet;
+import java.util.ArrayList;
+
+public class bulletController {
+
+    private ArrayList<Bullet> bullets = new ArrayList<>();
+
+    public void addBullet(Bullet bullet){
+        bullets.add(bullet);
+    }
+
+    public void removeInactiveBullets(){
+        ArrayList<Bullet> temp = new ArrayList<>(bullets.size());
+        for (Bullet bullet: bullets) {
+            if(bullet.isBulletStatus()){
+                temp.add(bullet);
+            }
+        }
+        bullets = temp;
+    }
+}
