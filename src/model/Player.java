c42babd68a7956437f88f8d97c7548e216739bca
Ilnar Shafigullin <shafigullin.ik@gmail.com>
2019-03-25 16:09:49 +0300

    class Player - default constants added;

--- src/model/Player.java
+++ src/model/Player.java
@@ -7,9 +7,21 @@ public class Player {
 
     private final Tank tank;
 
-    public Player(String name, Tank tank) {
+    private static final int DEFAULT_TANK_SIZE = 50;
+
+    private static final int DEFAULT_TANK_HEALTH = 3;
+
+    private static final int DEFAULT_TANK_STEP = 10;
+
+
+    public Player(String name, Point startPoint, int health, int step, int tankSize ) {
         this.name = name;
-        this.tank = tank;
+        this.tank = new Tank(startPoint, Directions.UP, health, step, tankSize, name);
+    }
+
+    public Player(String name, Point startPoint){
+        this.name = name;
+        this.tank = new Tank(startPoint, Directions.UP, DEFAULT_TANK_HEALTH, DEFAULT_TANK_STEP, DEFAULT_TANK_SIZE, name);
     }
 
     public String getName() {
