57467e4718516883e5799291d7a5381ed5c9c0b5
Ilnar Shafigullin <shafigullin.ik@gmail.com>
2019-03-25 20:12:58 +0300

    Class Main - tank coords fixed;

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,8 @@ public class Main {
     private static final int cellSize = 50;
 
     public static void main(String[] args) {
-        Player player1 = new Player("Vasya", new Point(5,5));
-        Player player2 = new Player("Petya", new Point(7,5));
+        Player player1 = new Player("Vasya", new Point(100,150));
+        Player player2 = new Player("Petya", new Point(200,155));
         Field field = new Field(DEFAULT_WINDOW_X_SIZE, DEFAULT_WINDOW_Y_SIZE , cellSize);
         Game game = new Game(player1, player2, field);
         JFrameView view = new JFrameView(game);
