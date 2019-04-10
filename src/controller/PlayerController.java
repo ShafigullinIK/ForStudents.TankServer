4378ef03f0148c58e2dddb58b568401e4f13a66b
ishafigu <shafigullin.ik@gmail.com>
2019-04-10 11:36:02 +0300

    class P;ayerController refactored;

--- src/controller/PlayerController.java
+++ src/controller/PlayerController.java
@@ -48,11 +48,11 @@ public class PlayerController extends KeyAdapter {
                 move(Directions.UP);
                 break;
             case KeyEvent.VK_SPACE:
-                if(tank.getBulletsNumber() > 0){
-                    Bullet b = tank.makeShot();
-                    bulletController.addBullet(b);
-                    break;
-                }
+//                if(tank.getBulletsNumber() > 0){
+//                    Bullet b = tank.makeShot();
+//                    bulletController.addBullet(b);
+//                    break;
+//                }
 
         }
 
